3bdc8569c9da4cf893fa2a9637e016d5d779d9d7
Chris Coffey <cmcof273@gmail.com>
2019-07-14 16:39:46 -0400

    Centaur tests

--- mythical-creatures/lib/centaur.rb
+++ mythical-creatures/lib/centaur.rb
@@ -2,27 +2,34 @@ class Centaur
   attr_reader :name
   attr_reader :breed
 
-  @cranky = false
-  @position = :standing
-  def initialize(name, breed)
+  NO = "NO!"
 
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+    @action_count = 0
+    @position = :standing
   end
 
   def run
-    positional_try :shoot do
-      @cranky ? "NO!!!" : "Clop clop clop clop!!!"
+    positional_try :run do
+      res = cranky? ? NO : "Clop clop clop clop!!!"
+      @action_count+=1
+      res
     end
   end
 
   def shoot
     positional_try :shoot do
-      @cranky ? "NO!!!" : "Twang!!!"
+      res = cranky? ? NO : "Twang!!!"
+      @action_count+=1
+      res
     end
   end
 
   def sleep
     positional_try :sleep do
-      @cranky = false
+      @action_count = 0
     end
   end
 
@@ -32,22 +39,37 @@ class Centaur
     end
   end
 
-  def cranky?
-    @cranky
+  def stand_up
+    positional_try :stand_up do
+      @position = :standing
+    end
   end
 
+  def cranky?
+    @action_count >= 3
+  end
+
+  def standing?
+    @position == :standing
+  end
+
+  def laying?
+    @position == :laying
+  end
 
 private
   def positional_try (action)
     standing_actions = [:run, :shoot, :lay_down]
-
+    laying_actions = [:stand_up, :sleep]
     if (standing_actions.include? action) &&
        (@position == :standing)
       yield
-    else if (@position == :laying)
-      yield
+    elsif (laying_actions.include? action) &&
+          (@position == :laying)
+     yield
     else
-      return "No!!!"
+      return NO
     end
   end
+
 end
